28699efe56d426d126aa19a9a3b66ff512e85ad1
thatpanda <thatpanda@users.noreply.github.com>
2015-02-01 22:44:16 -0800

    use case-insensitive check to filter file extension

--- src/ApkFilter.java
+++ src/ApkFilter.java
@@ -5,25 +5,19 @@ import javax.swing.filechooser.FileFilter;
 public class ApkFilter extends FileFilter
 {
     public boolean accept(File f) {
-        if (f.isDirectory()) {
+        if(f.isDirectory()) {
             return true;
         }
         
         String extension = getFileExtension(f);
-        if (extension != null) {
-            if (extension.equals(".apk") ||
-                extension.equals(".bin")) {
-                return true;
-            }
-        }
- 
-        return false;
+        return extension.equalsIgnoreCase(".apk") ||
+                extension.equalsIgnoreCase(".bin");
     }
 
     public String getDescription() {
         return "Android Application Package (*.apk; *.bin)";
     }
-    
+
     private static String getFileExtension(File f) {
         String filename = f.getName();
         int dotIndex = filename.lastIndexOf(".");
